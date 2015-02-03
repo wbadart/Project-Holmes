3fdd12b93ebc394fc7f657e6f04240586e2f07e9
wbadart <wbadart@nd.edu>
2015-02-03 01:18:40 -0500

    Loaded password library Script now compares user password to a collection of 10,000 common passwords before attempting a brute-force crack.  Also added some comments.

--- cracker.m
+++ cracker.m
@@ -19,36 +19,48 @@ clc;
 
 realpass = input('What is the password: ', 's');
 
-%% Password is a number
+%% Import Common Password Library
 
+filename = 'commonPass.txt';
+delimiter = '';
+formatSpec = '%s%[^\n\r]';
+fileID = fopen(filename,'r');
+dataArray = textscan(fileID, formatSpec, 'Delimiter', delimiter, 'EmptyValue' ,NaN, 'ReturnOnError', false);
+fclose(fileID);
+password = dataArray{:, 1};
+clearvars filename delimiter formatSpec fileID dataArray ans;
 
+%% Set Parameters
 
-if isa(realpass, 'double');
-    guess = 0;
-    counter = 0;
-    
-    while realpass ~= guess
-        guess = guess + 1;
+alphabet = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()-_=+[{]}\|";:/?.>'',<ÇüéâäàåçêëèïîìÄÅÉæÆôöòûùÿÖÜ¢£¥?ƒáíóúñÑªº¿¬½¼¡«»ßµ°·²';
+alphasize = size(alphabet);
+guess = ' ';
+counter = 0;
+i = 0;
+j = 0;
+
+%% Crack Password
+
+while strcmp(guess, realpass) == 0
+
+    % checks password against the library
+    for i = 1:size(password)
         counter = counter + 1;
+        guess = password(i);
+        if strcmp(guess, realpass) == 1
+            guess = guess{:};
+            break
+        end
     end
+
+    if strcmp(guess, realpass) == 1
+        break
+    end
+
+    guess = ' '; % reset 'guess.'  It wasn't a common password
     
-    disp(['Got it.  The password is ', num2str(guess)]);
-    disp(['It took ', num2str(counter), ' guesses.']);
-    
-    
-    % Password is a word
-    
-elseif isa(realpass, 'char');
-    alphabet = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()-_=+[{]}\|";:/?.>'',<ÇüéâäàåçêëèïîìÄÅÉæÆôöòûùÿÖÜ¢£¥?ƒáíóúñÑªº¿¬½¼¡«»ßµ°·²';
-    alphasize = size(alphabet);
-    guess = ' ';
-    counter = 0;
-    i = 0;
-    j = 0;
-    
-    while strcmp(guess, realpass) == 0
-        
-        
+    % check all 1 character passwords possible w/ given alphabet
+    if strcmp(guess, realpass) == 0
         for i = 1:alphasize(2)
             guess(1) = alphabet(i);
             counter = counter + 1;
@@ -56,82 +68,91 @@ elseif isa(realpass, 'char');
                 break
             end
         end
-        
-        if strcmp(guess, realpass) == 0
-            for i = 1:alphasize(2)
-                counter = counter + 1;
-                guess(1) = alphabet(i);
-                for j = 1:alphasize(2)
-                    counter = counter + 1;
-                    guess(2) = alphabet(j);
-                    if strcmp(guess, realpass) == 1
-                        break
-                    end
-                end
-                if strcmp(guess, realpass) == 1
-                    break
-                end
-            end
-        end
-        
-        if strcmp(guess, realpass) == 0
-            for i = 1:alphasize(2)
-                counter = counter + 1;
-                guess(1) = alphabet(i);
-                for j = 1:alphasize(2)
-                    counter = counter + 1;
-                    guess(2) = alphabet(j);
-                    for k = 1:alphasize(2)
-                        counter = counter + 1;
-                        guess(3) = alphabet(k);
-                        if strcmp(guess, realpass) == 1
-                            break
-                        end
-                    end
-                    if strcmp(guess, realpass) == 1
-                        break
-                    end
-                end
-                if strcmp(guess, realpass) == 1
-                    break
-                end
-            end
-        end
-        
-        if strcmp(guess, realpass) == 0
-            for i = 1:alphasize(2)
-                counter = counter + 1;
-                guess(1) = alphabet(i);
-                for j = 1:alphasize(2)
-                    counter = counter + 1;
-                    guess(2) = alphabet(j);
-                    for k = 1:alphasize(2)
-                        counter = counter + 1;
-                        guess(3) = alphabet(k);
-                        for l = 1:alphasize(2)
-                            counter = counter + 1;
-                            guess(4) = alphabet(l);
-                            if strcmp(guess, realpass) == 1
-                                break
-                            end
-                        end
-                        if strcmp(guess, realpass) == 1
-                            break
-                        end
-                    end
-                    if strcmp(guess, realpass) == 1
-                        break
-                    end
-                end
-                if strcmp(guess, realpass) == 1
-                    break
-                end
-            end
-        end
-
     end
     
-    disp(['Got it.  The password is ', guess]);
-    disp(['It took ', num2str(counter), ' guesses.']);
+    % the password has more than one characters.  Check all possible 2
+    % character passwords
+    if strcmp(guess, realpass) == 0
+        for i = 1:alphasize(2)
+            counter = counter + 1;
+            guess(1) = alphabet(i);
+            for j = 1:alphasize(2)
+                counter = counter + 1;
+                guess(2) = alphabet(j);
+                if strcmp(guess, realpass) == 1
+                    break
+                end
+            end
+            if strcmp(guess, realpass) == 1
+                break
+            end
+        end
+    end
     
+    % 3 character passwords
+    if strcmp(guess, realpass) == 0
+        for i = 1:alphasize(2)
+            counter = counter + 1;
+            guess(1) = alphabet(i);
+            for j = 1:alphasize(2)
+                counter = counter + 1;
+                guess(2) = alphabet(j);
+                for k = 1:alphasize(2)
+                    counter = counter + 1;
+                    guess(3) = alphabet(k);
+                    if strcmp(guess, realpass) == 1
+                        break
+                    end
+                end
+                if strcmp(guess, realpass) == 1
+                    break
+                end
+            end
+            if strcmp(guess, realpass) == 1
+                break
+            end
+        end
+    end
+    
+    % 4 character passwords
+    if strcmp(guess, realpass) == 0
+        for i = 1:alphasize(2)
+            counter = counter + 1;
+            guess(1) = alphabet(i);
+            for j = 1:alphasize(2)
+                counter = counter + 1;
+                guess(2) = alphabet(j);
+                for k = 1:alphasize(2)
+                    counter = counter + 1;
+                    guess(3) = alphabet(k);
+                    for l = 1:alphasize(2)
+                        counter = counter + 1;
+                        guess(4) = alphabet(l);
+                        if strcmp(guess, realpass) == 1
+                            break
+                        end %ends if
+                    end %ends for(l)
+                    if strcmp(guess, realpass) == 1
+                        break
+                    end %ends if
+                end %ends for(k)
+                if strcmp(guess, realpass) == 1
+                    break
+                end %ends if
+            end %ends for(j)
+            if strcmp(guess, realpass) == 1
+                break
+            end %ends if
+        end %ends for(i)
+    end %ends if strcmp(guess, ...
+
+end %ends while
+
+%% Display Results
+
+disp(['Got it.  The password is "', guess, '".']);
+if counter == 1
+    disp(['It took ', num2str(counter), ' guess.']);
+else
+    disp(['It took ', num2str(counter), ' guesses.']);
 end
