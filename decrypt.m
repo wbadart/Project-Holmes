4ed65acde7062afa703a4da7bcd992c55dc799fb
wbadart <wbadart@nd.edu>
2015-02-17 21:58:06 -0500

    Finished first phase The script can now decrypt messages that don't have letters that occur the same number of times.  That's the next step.  For now, it works as planned (phew) for re-scrambling letters based on frequency.

--- decrypt.m
+++ decrypt.m
@@ -32,18 +32,46 @@ end
 
 %% Rank message letters based on frequency
 
-for i = 1:length(alphabetF)
+for k = 1:length(alphabetF)
     maxIndex = find(alphabetF == max(alphabetF));
-    commonM(i).rank = i;
-    commonM(i).letter = alphabet(maxIndex);
+    commonM(k).rank = k;
+    commonM(k).letter = alphabet(maxIndex);
     alphabetF(maxIndex) = 0;
 end
 
 %% Assign message new letters based on rank
 
-for i = 1:length(alphabet)
-    jt = message(i);
-    at = find(jt == commonM.letter);
+% %remove ties in frequency
+% for j = 1:length(alphabet)
+%     if length(commonM(j).letter) > 1
+%         commonM(j + 1).letter = commonM(j).letter(2:end);
+%         commonM(j).letter = commonM(j).letter(1);
+%     end
+% end
+
+%removes unneeded elements of commonM
+o = 1;
+while length(commonM(o).letter) ~= 26
+    lineup(o).value = commonM(o).letter;
+    o = o + 1;
+end
+
+%arranges letters of message according to frequency
+for p = 1:length(lineup)
+    for q = 1:length(lineup(p).value)
+        lineupM(p) = lineup(p).value(q);
+    end
+end
+
+%adds field letterNew to commonM structure
+for m = 1:length(alphabet)
+    commonM(m).letterNew = common(m).letter;
+end
+
+%maps new letters to message according to frequency
+for r = 1:length(lineupM)
+    index = strfind(message, lineupM(r));
+    message(index) = commonM(r).letterNew;
 end
 
 
